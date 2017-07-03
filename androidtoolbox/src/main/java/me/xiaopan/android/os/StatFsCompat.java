5a9ca975380e67bce7209338d4c0e4eaa7c60ab1
Flyge <sky@xiaopan.me>
2017-07-03 16:15:02 +0800

    Update StatFsCompat

--- androidtoolbox/src/main/java/me/xiaopan/android/os/StatFsCompat.java
+++ androidtoolbox/src/main/java/me/xiaopan/android/os/StatFsCompat.java
@@ -22,7 +22,7 @@ import android.os.StatFs;
 
 /**
  * <h4>StatFs兼容类<h4/>
- *
+ * <p>
  * 由于StatFs的API从API18之后发生了较大的变化，主要是Android官方认识到之前的API用起来不方便并且有坑，于是新添加了几个方法来替代旧的方法
  */
 public class StatFsCompat {
@@ -33,51 +33,13 @@ public class StatFsCompat {
     }
 
     /**
-     * 获取可用字节数
-     * @return 可用字节数，单位byte
-     */
-    @SuppressWarnings("deprecation")
-    public long getAvailableBytes(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
-            return statFs.getAvailableBytes();
-        }else{
-            return (long) statFs.getAvailableBlocks() * statFs.getBlockSize();
-        }
-    }
-
-    /**
-     * 获取总字节数
-     * @return 总字节数，单位byte
-     */
-    @SuppressWarnings("deprecation")
-    public long getTotalBytes(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
-            return statFs.getTotalBytes();
-        }else{
-            return (long) statFs.getBlockCount() * statFs.getBlockSize();
-        }
-    }
-
-    /**
-     * 获取空闲字节数
-     * @return 空闲字节数，单位byte
-     */
-    @SuppressWarnings("deprecation")
-    public long getFreeBytes(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
-            return statFs.getFreeBytes();
-        }else{
-            return (long) statFs.getFreeBlocks() * statFs.getBlockSize();
-        }
-    }
-
-    /**
      * 创建一个用于查询外部存储（Environment.getExternalStorageDirectory()）容量的StatFsCompat
+     *
      * @return null：外部存储不可用
      */
     @SuppressWarnings("deprecation")
-    public static StatFsCompat newByExternalStorage(){
-        if(!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+    public static StatFsCompat newByExternalStorage() {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             return null;
         }
         return new StatFsCompat(Environment.getExternalStorageDirectory().getPath());
@@ -85,10 +47,53 @@ public class StatFsCompat {
 
     /**
      * 创建一个用于查询内部存储（/data）容量的StatFsCompat
+     *
      * @return null：内部存储不可用
      */
     @SuppressWarnings("deprecation")
-    public static StatFsCompat newByInternalStorage(){
+    public static StatFsCompat newByInternalStorage() {
         return new StatFsCompat("/data");
+    }
+
+    /**
+     * 获取可用字节数
+     *
+     * @return 可用字节数，单位byte
+     */
+    @SuppressWarnings("deprecation")
+    public long getAvailableBytes() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return statFs.getAvailableBytes();
+        } else {
+            return (long) statFs.getAvailableBlocks() * statFs.getBlockSize();
+        }
+    }
+
+    /**
+     * 获取总字节数
+     *
+     * @return 总字节数，单位byte
+     */
+    @SuppressWarnings("deprecation")
+    public long getTotalBytes() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return statFs.getTotalBytes();
+        } else {
+            return (long) statFs.getBlockCount() * statFs.getBlockSize();
+        }
+    }
+
+    /**
+     * 获取空闲字节数
+     *
+     * @return 空闲字节数，单位byte
+     */
+    @SuppressWarnings("deprecation")
+    public long getFreeBytes() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return statFs.getFreeBytes();
+        } else {
+            return (long) statFs.getFreeBlocks() * statFs.getBlockSize();
+        }
     }
 }
